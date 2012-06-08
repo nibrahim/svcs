c0363ffe695399abddc6b6bd011a4af0d89b648f
Noufal Ibrahim <noufal@nibrahim.net.in>
2012-06-08 19:30:05 +0530

    Added a simple FileStorage class to the storage module.
    M	svc/storage.py
    A simple FileStorage class was added that creates the storage
    directory when initialised. All tests pass.

--- svc/storage.py
+++ svc/storage.py
@@ -1,1 +1,10 @@
+import os
 
+class FileStorage(object):
+    def __init__(self, location):
+        "Initialises a FileStorage at the given location"
+        self.location = location
+        os.mkdir(location)
+
+        
+        
