66dd9ade061cd4aa9752934e037025160b0b4ee3
Noufal Ibrahim <noufal@nibrahim.net.in>
2012-06-10 16:01:28 +0530

    Adds code to actually serialise and store the file.
    M	svc/storage/file_backend.py
    Adds a store_object method that serialises the given object and stores
    it in the directory.

--- svc/storage/file_backend.py
+++ svc/storage/file_backend.py
@@ -4,7 +4,7 @@ class Storage(object):
     def store_object(self, obj):
         raise NotImplementedError()
 
-    def get_object(address):
+    def get_object(self, address):
         raise NotImplementedError()
 
 
@@ -12,7 +12,20 @@ class FileStorage(Storage):
     def __init__(self, location):
         "Initialises a FileStorage at the given location"
         self.location = location
+        self.obj_dir = "objects"
         if not os.path.isdir(location):
             os.mkdir(location)
+
+    def store_object(self, obj):
+        "Stores the serialised object in storage directory."
+        address = obj.id
+        location = os.path.join(self.location, self.obj_dir, address)
+        content = obj.serialise()
+        f = open(location, "wb")
+        f.write(content)
+        f.close()
+        
+        
+
 
         
