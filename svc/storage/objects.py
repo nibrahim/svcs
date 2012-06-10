983463b7304cbf2d521c2a55d8fcf72d5d28cc38
Noufal Ibrahim <noufal@nibrahim.net.in>
2012-06-10 15:07:38 +0530

    Adds id method to Object and File.
    M	svc/storage/objects.py
    Adds an abstract id method to Object and a concrete implemntation in
    File.

--- svc/storage/objects.py
+++ svc/storage/objects.py
@@ -1,5 +1,14 @@
-class Object(object): pass
+import hashlib
+
+class Object(object): 
+    def id(self):
+        raise NotImplementedError()
 
 class File(Object): 
     def __init__(self, contents):
         self.contents = contents
+
+    @property
+    def id(self):
+        return hashlib.sha1(self.contents).hexdigest()
+        
