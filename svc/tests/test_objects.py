f90e4f13ef25716f5a7e5d6ee331ea75575ce569
Noufal Ibrahim <noufal@nibrahim.net.in>
2012-06-10 16:30:40 +0530

    Adds a test to verify serialisation of File objects
    M	svc/tests/test_objects.py
    This adds the test_serialise_file function to this file.

--- svc/tests/test_objects.py
+++ svc/tests/test_objects.py
@@ -1,3 +1,4 @@
+import base64
 import hashlib
 
 from ..storage import File
@@ -15,4 +16,14 @@ def test_address_file():
 
     expected_hash = hashlib.sha1("This is a test string").hexdigest()
     assert f.id == expected_hash
+
+def test_serialise_file():
+    """Verifies the a File object can serialise itself. 
+
+    Assumes knowledge about the serialisation format."""
+
+    f = File("This is a test string")
+    
+    expected_content = base64.b64encode("This is a test string")
+    f.serialise() == expected_content
 
