6beee7e461a662a021741e71a2de5ff2b8df5f54
Noufal Ibrahim <noufal@nibrahim.net.in>
2012-06-10 13:24:21 +0530

    Adds a `test_create_file` test.
    M	svc/tests/test_file_storage.py
    
    Adds a test_create_file test which tries to instantiate a File class
    and test basic attributes.

--- svc/tests/test_file_storage.py
+++ svc/tests/test_file_storage.py
@@ -2,7 +2,7 @@ import glob
 import os
 import shutil
 
-from ..storage import FileStorage
+from ..storage import FileStorage, File
 
 def test_initialisation():
     "Initialises a storage object"
@@ -19,21 +19,11 @@ def test_init_existing():
     shutil.rmtree("/tmp/test")
 
 def test_create_file(file_store):
-    # Prepare the file
-    f = open("/tmp/input_file", "wb")
-    f.write("\nThis is a test\n")
-    f.close()
-    
-    # Ask the Storage object to save the file.
-    file_store.create_file(f)
-    
-    # Manually check if it has been saved This part pokes into the
-    # store directly using undocumented variables.
-    file_storage_dir = file_store.location + "/files/"
-    repo_file = glob.glob(file_storage_dir + "/*")[0]
-    file_contents = open(repo_file).read()
-    assert file_contents == "\nThis is a test\n"
+    """Creates a file object and tests whether it is storing the
+    contents"""
+    f = File("This is a test string")
 
+    assert f.contents == "This is a test string"
     
     
     
