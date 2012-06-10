31f2a2e99be7a78d0d7b1bfe586e90ce9eaa40b1
Noufal Ibrahim <noufal@nibrahim.net.in>
2012-06-10 15:40:36 +0530

    Moves interface methods to Storage base class.
    M	svc/storage/file_backend.py
    
    Creates a new Storage base class that publishes our Storage interface.

--- svc/storage/file_backend.py
+++ svc/storage/file_backend.py
@@ -1,11 +1,18 @@
 import os
 
-class FileStorage(object):
+class Storage(object):
+    def store_object(self, obj):
+        raise NotImplementedError()
+
+    def get_object(address):
+        raise NotImplementedError()
+
+
+class FileStorage(Storage):
     def __init__(self, location):
         "Initialises a FileStorage at the given location"
         self.location = location
         if not os.path.isdir(location):
             os.mkdir(location)
 
-        
         
