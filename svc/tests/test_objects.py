96752e7bdedbd65e1a5588e73c3ab328d5076221
Noufal Ibrahim <noufal@nibrahim.net.in>
2012-06-11 15:33:37 +0530

    Adds a test that creates a commit.
    M	svc/tests/test_objects.py
    
    Adds a new test `test_create_commit` which instantiates a commit and
    pokes inside it to make sure that everything is stored properly.

--- svc/tests/test_objects.py
+++ svc/tests/test_objects.py
@@ -1,7 +1,8 @@
 import base64
+import datetime
 import hashlib
 
-from ..storage import File
+from ..storage import File, Commit
 
 def test_create_file():
     """Creates a file object and tests whether it is storing the
@@ -40,5 +41,29 @@ def test_load_file():
     f1 = File.load(s)
     assert f0.contents == f1.contents
     
+def test_create_commit():
+    """
+    Tries to create a commit and checks whether the contents are
+    stored properly.
+    """
+    # List of files to commit.
+    files = [("file1.txt", File("Contents of file 1")),
+             ("file2.txt", File("Contents of file 2")),
+             ("file3.txt", File("Contents of file 3"))]
+    message = "Commit message"
+    date = datetime.datetime.now()
+    committer = "noufal@nibrahim.net.in"
+    parent_commit = None
+    c = Commit(committer, message, date, parent_commit, *files)
+
+    assert c.date == date
+    assert c.committer == committer
+    assert c.parent == parent_commit
+
+    expected_files = [(x, y.id) for x,y in files]
+    assert c.files == expected_files
     
+
+
+
     
