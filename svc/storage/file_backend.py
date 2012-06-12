452825ba083cb677e641fab8eb523ea6c474a742
Noufal Ibrahim <noufal@nibrahim.net.in>
2012-06-12 10:39:09 +0530

    Adds update_tip method to FileStorage
    M	svc/storage/file_backend.py
    Adds the update_tip to FileStorage to make the test pass. Removes some
    unnecessary operations spread out through the code to the constructor.

--- svc/storage/file_backend.py
+++ svc/storage/file_backend.py
@@ -14,15 +14,19 @@ class FileStorage(Storage):
     def __init__(self, location):
         "Initialises a FileStorage at the given location"
         self.location = location
-        self.obj_dir = "objects"
+        self.obj_dir = os.path.join(self.location,"objects")
+        self.tip = os.path.join(self.location, "TIP")
         if not os.path.isdir(location):
             os.mkdir(location)
-            os.mkdir(os.path.join(self.location, self.obj_dir))
+            os.mkdir(os.path.join(self.obj_dir))
+            with open(self.tip, "wb") as f:
+                pass
+            
 
     def store_object(self, obj):
         "Stores the serialised object in storage directory."
         address = obj.id
-        location = os.path.join(self.location, self.obj_dir, address)
+        location = os.path.join(self.obj_dir, address)
         content = obj.serialise()
         f = open(location, "wb")
         f.write(content)
@@ -30,11 +34,18 @@ class FileStorage(Storage):
     
     def get_object(self, address):
         "Retrieves the object with the given address."
-        location = os.path.join(self.location, self.obj_dir, address)
+        location = os.path.join(self.obj_dir, address)
         with open(location) as f:
             contents = f.read()
         f = Object.load(contents)
         return f
+    
+    def update_tip(self, commit):
+        "Updates the tip to the given commit"
+        with open(self.tip, "wb") as f:
+            f.write(commit.id)
+            
+            
         
 
 
